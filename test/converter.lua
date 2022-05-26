fac5796ce99a97b063100cc73fee92546f7c3c9b
Svarog <alexander.s.zernov@gmail.com>
2022-05-26 21:22:47 +1200

    Update converter.lua

--- test/converter.lua
+++ test/converter.lua
@@ -1,5 +1,5 @@
-local databank1 = Slot1
-local databank2 = Slot2
+local databank1 = slot1
+local databank2 = slot2
 
 local keyListString = databank1.getKeys()
 local keyList = {}
@@ -8,16 +8,16 @@ if keyListString and keyListString ~= "" then
 end
 
 for _, id in ipairs(keyList) do
-	local userObjectString = databankSlot.getStringValue(id)
+	local userObjectString = databank1.getStringValue(id)
 	if userObjectString and userObjectString ~= "" then
 		local userObject = json.decode(userObjectString)
 		local newObject = {}
 		newObject[1] = id
 		newObject[2] = userObject.name or "Unknown"
 		newObject[3] = math.floor(userObject.time) or 0
-		newObject[4] = math.floor(userObject.lastTime) or newObject[3]
+		newObject[4] = userObject.lastTime and math.floor(userObject.lastTime) or newObject[3]
 		newObject[5] = userObject.counter or 0
-		local srtingToRecord = table.concat(newObject,","))
+		local srtingToRecord = table.concat(newObject,",")
 		databank2.setStringValue(id,srtingToRecord)
 	end
 end
