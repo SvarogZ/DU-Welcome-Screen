77941d811140522ce94aac99a3138a8f0b028c0d
Svarog <alexander.s.zernov@gmail.com>
2022-05-26 21:59:11 +1200

    Update converter.lua

--- test/converter.lua
+++ test/converter.lua
@@ -10,18 +10,19 @@ end
 local limit = 100
 local i = 1
 for _, id in ipairs(keyList) do
-	if i > 100 then break end
-	i = i+1
-	local userObjectString = databank1.getStringValue(id)
-	if userObjectString and userObjectString ~= "" then
-		local userObject = json.decode(userObjectString)
-		local newObject = {}
-		newObject[1] = id
-		newObject[2] = userObject.name or "Unknown"
-		newObject[3] = math.floor(userObject.time) or 0
-		newObject[4] = userObject.lastTime and math.floor(userObject.lastTime) or newObject[3]
-		newObject[5] = userObject.counter or 0
-		local srtingToRecord = table.concat(newObject,",")
-		databank2.setStringValue(id,srtingToRecord)
+	if tonumber(id) and i < 100 then
+		local userObjectString = databank1.getStringValue(id)
+		if userObjectString and userObjectString ~= "" then
+			local userObject = json.decode(userObjectString)
+			local newObject = {}
+			newObject[1] = id
+			newObject[2] = userObject.name or "Unknown"
+			newObject[3] = userObject.time and math.floor(userObject.time) or 0
+			newObject[4] = userObject.lastTime and math.floor(userObject.lastTime) or newObject[3]
+			newObject[5] = userObject.counter or 0
+			local srtingToRecord = table.concat(newObject,",")
+			databank2.setStringValue(id,srtingToRecord)
+		end
 	end
+	i = i+1
 end
