54e15210c11f8cce5645d733dd97864e9b3ac8d8
Svarog <alexander.s.zernov@gmail.com>
2022-05-26 22:31:47 +1200

    Update welcome_screen.lua

--- welcome_screen.lua
+++ welcome_screen.lua
@@ -1,15 +1,31 @@
 setOutput("welcome")
 
-local json = require "dkjson"
+local function split(s,sep)
+    local sep = sep or ","
+    local result = {}
+    local i = 1
+    for c in s:gmatch(string.format("([^%s]+)", sep)) do
+        local n = tonumber(c)
+        if n then
+            result[i] = n
+        else
+            result[i] = c
+        end
+        i = i + 1
+    end
+    return result
+end
+
 local stringForm = getInput()
-local data = json.decode(stringForm)
+logMessage("stringForm is "..stringForm)
+local data = stringForm and type(stringForm)=="string" and split(stringForm,",")
 
 local stringToShow = "Welcome"
-if data and data[1] then
+if data and data[1] and data[1] ~= "" then
 	stringToShow = stringToShow .. ", " .. data[1] .. "!"
 end
 
-if data and data[2] then
+if data and data[2] and type(data[2])=="number" then
 	local function dateFormat(t)
 		local t = type(t)=='number' and t>0 and t or 0
 		local text = ""
@@ -27,7 +43,7 @@ if data and data[2] then
 
 		return text..minute.."m"
 	end
-
+	
 	stringToShow = stringToShow .. "\n\n You visited us\n" .. dateFormat(data[2]) .. " ago."
 else
 	stringToShow = stringToShow .. "\n\n This is your first visit."
