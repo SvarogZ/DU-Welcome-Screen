c41b6fb80b210cda04ee0ac4c37f33445772a17f
Svarog <alexander.s.zernov@gmail.com>
2022-05-27 14:45:32 +1200

    Create converter2.lua

---
+++ test/converter2.lua
@@ -0,0 +1,43 @@
+local databank1 = slot1
+local databank2 = slot2
+
+function string:split(sep)
+    local sep = sep or ","
+    local result = {}
+    local i = 1
+    for c in self:gmatch(string.format("([^%s]+)", sep)) do
+        local n = tonumber(c)
+        if n then
+            result[i] = n
+        else
+            result[i] = c
+        end
+        i = i + 1
+    end
+    return result
+end
+
+local keyListString = databank1.getKeys()
+local keyList = {}
+if keyListString and keyListString ~= "" then
+	keyList = json.decode(keyListString)
+end
+
+local limit = 100
+local i = 1
+for _, id in ipairs(keyList) do
+	if tonumber(id) and i < limit then
+		local userObjectString = databank1.getStringValue(id)
+		if userObjectString and userObjectString ~= "" then
+			local userObject = userObjectString:split(",")
+			local newObject = {}
+			newObject[1] = userObject[2]
+			newObject[2] = userObject[3]
+			newObject[3] = userObject[4]
+			newObject[4] = userObject[5]
+			local srtingToRecord = table.concat(newObject,",")
+			databank2.setStringValue(id,srtingToRecord)
+		end
+	end
+	i = i+1
+end
