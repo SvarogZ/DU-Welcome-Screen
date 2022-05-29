3ae1ff50400bff81a617787d4fb4810310d80235
Svarog <alexander.s.zernov@gmail.com>
2022-05-29 18:14:07 +1200

    Update user_adder2.lua

--- test/user_adder2.lua
+++ test/user_adder2.lua
@@ -1,47 +1,67 @@
 local add = 100
-local databank1 = slot1
---databank1.clear()
+local maxUsersForDatabank = 600
+local clearDatabanks = false
 
-function string:split(sep)
-    local sep = sep or ","
-    local result = {}
-    local i = 1
-    for c in self:gmatch(string.format("([^%s]+)", sep)) do
-        local n = tonumber(c)
-        if n then
-            result[i] = n
-        else
-            result[i] = c
-        end
-        i = i + 1
-    end
-    return result
+local databanks = {}
+
+local function initiateSlots()
+	for _, slot in pairs(unit) do
+		if type(slot) == "table" and type(slot.export) == "table" and slot.getElementClass then
+			local elementClass = slot.getElementClass():lower()
+			if elementClass == "databankunit" then
+				table.insert(databanks,slot)
+			end
+		end
+	end
+	
+	if #databanks < 1 then
+		error("No databank connected!")
+	end
+	
+	table.sort(databanks, function (a, b) return (a.getId() < b.getId()) end)
 end
 
-local keyListString = databank1.getKeys()
---system.print(keyListString)
-local keyList = {}
-if keyListString and keyListString ~= "" then
-	--keyList = json.decode(keyListString)
-	--local replaced = keyListString:gsub('[[]', '{'):gsub('[]]', '}'):gsub('"(%w+)":', '["%1"]=')
-	--keyList = load('return ' .. replaced)()
-	keyList = keyListString:gsub([[([%[%]"])]],""):split(",")
-end
-system.print(#keyList)
-table.sort(keyList, function(a,b) return tonumber(a) > tonumber(b) end)
-local maxKey = tonumber(keyList[1])
---system.print("maxKey1="..maxKey)
-if not maxKey then maxKey = 100000 end
---system.print("maxKey2="..maxKey)
+initiateSlots()
 
-for i=1, add do
-	local nextKey = maxKey + i
-	local newObject = {}
-	newObject[1] = "U"..nextKey
-	newObject[2] = 123
-	newObject[3] = 123456789
-	newObject[4] = 123456789
-	local srtingToRecord = table.concat(newObject,",")
-	databank1.setStringValue(nextKey,srtingToRecord)
-	--system.print("nextKey="..nextKey.." recorded")
+if clearDatabanks then
+	for _, databank in ipairs(databanks) do
+		databank.clear()
+	end
+end
+
+local databankToRecod
+local maxKey = 100000
+
+for _, databank in ipairs(databanks) do
+	local keyListString = databank.getKeys()
+	--system.print(keyListString)
+	local keyList = {}
+	if keyListString and keyListString ~= "" then
+		local i = 1
+		for c in keyListString:gmatch('%d+') do
+			keyList[i] = tonumber(c)
+			i = i + 1
+		end
+		system.print("records: ".. (i-1))
+		if i-1 <= maxUsersForDatabank - add then
+			databankToRecod = databank
+			table.sort(keyList, function(a,b) return a > b end)
+			maxKey = keyList[1] or maxKey
+			break
+		end
+	end
+end
+
+if databankToRecod then
+	for i=1, add do
+		local nextKey = maxKey + i
+		local newObject = {}
+		newObject[1] = "U"..nextKey
+		newObject[2] = 123
+		newObject[3] = 123456789
+		newObject[4] = 123456789
+		local srtingToRecord = table.concat(newObject,",")
+		databankToRecod.setStringValue(nextKey,srtingToRecord)
+		--system.print("nextKey="..nextKey.." recorded")
+	end
 end
