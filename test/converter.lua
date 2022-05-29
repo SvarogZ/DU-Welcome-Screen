ec36b2393af63840143af33dc76a89b34f13219b
Svarog <alexander.s.zernov@gmail.com>
2022-05-29 19:34:42 +1200

    Update converter.lua

--- test/converter.lua
+++ test/converter.lua
@@ -4,22 +4,24 @@ local databank2 = slot2
 local keyListString = databank1.getKeys()
 local keyList = {}
 if keyListString and keyListString ~= "" then
-	keyList = keyListString:gsub([[([%[%]"])]],""):split(",")
+	local i = 1
+	for c in keyListString:gmatch('%d+') do
+		keyList[i] = tonumber(c)
+		i = i + 1
+	end
 end
 
-local limit = 100
 local i = 1
 for _, id in ipairs(keyList) do
-	if tonumber(id) and i < limit then
+	if tonumber(id) then
 		local userObjectString = databank1.getStringValue(id)
 		if userObjectString and userObjectString ~= "" then
 			local userObject = json.decode(userObjectString)
 			local newObject = {}
-			newObject[1] = id
-			newObject[2] = userObject.name or "Unknown"
+			newObject[1] = userObject.name or "Unknown"
+			newObject[2] = userObject.counter or 0
 			newObject[3] = userObject.time and math.floor(userObject.time) or 0
 			newObject[4] = userObject.lastTime and math.floor(userObject.lastTime) or newObject[3]
-			newObject[5] = userObject.counter or 0
 			local srtingToRecord = table.concat(newObject,",")
 			databank2.setStringValue(id,srtingToRecord)
 		end
